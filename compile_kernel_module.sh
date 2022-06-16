9c6266068fdc8677ac8b610eabd846f4cfe0d992
bobaaldering <492152@student.saxion.nl>
2022-06-16 14:00:52 +0200

    Created a simple shell script for creating object files.

---
+++ compile_kernel_module.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DIR="build" # Build directory.
+
+# Check if the folder already exists.
+if [ -d "$DIR" ]; then
+	cd DIR || exit
+# Otherwise, make the
+else
+	mkdir DIR
+  cd DIR || exit
+fi
+
+make -C /usr/src/"$(uname -r)"/ M="$(pwd)"
+make load
