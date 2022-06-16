99f51da2e57da9a9317a05cb476da50245536f66
Bob Aaldering <492152@student.saxion.nl>
2022-06-16 14:37:24 +0200

    It is now possible to work with the shell script.

--- compile_kernel_module.sh
+++ compile_kernel_module.sh
@@ -4,12 +4,12 @@ DIR="build" # Build directory.
 
 # Check if the folder already exists.
 if [ -d "$DIR" ]; then
-	cd DIR || exit
+	cd "$(DIR)" || exit
 # Otherwise, make the
 else
-	mkdir DIR
-  cd DIR || exit
+	mkdir "$(DIR)"
+  cd "$(DIR)" || exit
 fi
 
-make -C /usr/src/linux-headers-"$(uname -r)"/ M="$(pwd)"
+make -C /usr/src/linux-headers-"$(uname -r)"/ M="$(pwd)" modules
 make load
